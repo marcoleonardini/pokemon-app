5582a28f86b8f832e195e2e47777ed541d32e154
Marco Leonardini <marco_leonardini@hotmail.com>
2020-04-20 22:11:00 -0400

    Added Pokemon Service

---
+++ lib/src/core/services/pokemon.service.dart
@@ -0,0 +1,26 @@
+import 'package:pokemon_app/src/core/const/endpoints.dart';
+import 'package:pokemon_app/src/core/models/list_pokemon.model.dart';
+
+import 'package:pokemon_app/src/core/models/pokemon.model.dart';
+
+import 'package:http/http.dart' as http;
+
+class PokemonService {
+  static Future<List<Pokemon>> getListPokemon() async {
+    var response = await http.get(kListPokemonEndpoint);
+    ListPokemon listPokemon = listPokemonFromJson(response.body);
+
+    List<Future<Pokemon>> futures = List.from(
+        listPokemon.results.map<Future<Pokemon>>((e) => getPokemon(e.url)));
+
+    List<Pokemon> res = await Future.wait(futures);
+    print(res.length);
+    return res;
+  }
+
+  static Future<Pokemon> getPokemon(url) async {
+    var response = await http.get(url);
+    Pokemon pokemon = pokemonFromJson(response.body);
+    return pokemon;
+  }
+}
